35ffa464926d233108303104e4960969c6cd1b5c
Stefan Hagen <stefan@hagen.link>
2024-01-03 00:50:51 +0100

    Added some starting init for the package (WIP)
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

---
+++ tallipoika/__init__.py
@@ -0,0 +1,78 @@
+"""Stableson (Finnish: tallipoika) - a JSON Canonicalization Scheme (JCS) implementation."""
+import datetime as dti
+import logging
+import os
+import pathlib
+from typing import List, no_type_check
+
+# [[[fill git_describe()]]]
+__version__ = '2024.01.03+parent.dirty'
+# [[[end]]] (checksum: fe0ff64f30b32840de892cd51734e37e)
+__version_info__ = tuple(
+    e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
+)
+
+APP_ALIAS = str(pathlib.Path(__file__).parent.name)
+APP_ENV = APP_ALIAS.upper()
+APP_NAME = locals()['__doc__']
+DEBUG = bool(os.getenv(f'{APP_ENV}_DEBUG', ''))
+VERBOSE = bool(os.getenv(f'{APP_ENV}_VERBOSE', ''))
+QUIET = False
+STRICT = bool(os.getenv(f'{APP_ENV}_STRICT', ''))
+ENCODING = 'utf-8'
+ENCODING_ERRORS_POLICY = 'ignore'
+DEFAULT_CONFIG_NAME = f'.{APP_ALIAS}.json'
+DEFAULT_LF_ONLY = 'YES'
+DEFAULT_STRUCTURE_NAME = 'structure.yml'
+DEFAULT_STRUCTURES_NAME = 'structures.yml'
+log = logging.getLogger()  # Module level logger is sufficient
+LOG_FOLDER = pathlib.Path('logs')
+LOG_FILE = f'{APP_ALIAS}.log'
+LOG_PATH = pathlib.Path(LOG_FOLDER, LOG_FILE) if LOG_FOLDER.is_dir() else pathlib.Path(LOG_FILE)
+LOG_LEVEL = logging.INFO
+VERSION = __version__
+VERSION_DOTTED_TRIPLE = '.'.join(__version_info__[:3])
+TS_FORMAT_LOG = '%Y-%m-%dT%H:%M:%S'
+TS_FORMAT_PAYLOADS = '%Y-%m-%d %H:%M:%S.%f UTC'
+TS_FORMAT_GENERATOR = '%Y-%m-%d %H:%M:%S.%f +00:00'
+
+__all__: List[str] = [
+    'APP_ALIAS',
+    'APP_ENV',
+    'APP_NAME',
+    'DEBUG',
+    'DEFAULT_CONFIG_NAME',
+    'DEFAULT_STRUCTURE_NAME',
+    'DEFAULT_STRUCTURES_NAME',
+    'ENCODING',
+    'TS_FORMAT_GENERATOR',
+    'VERSION',
+    'VERSION_DOTTED_TRIPLE',
+    'log',
+]
+
+
+@no_type_check
+def formatTime_RFC3339(self, record, datefmt=None):  # noqa
+    """HACK A DID ACK we could inject .astimezone() to localize ..."""
+    return dti.datetime.fromtimestamp(record.created, dti.timezone.utc).isoformat()  # pragma: no cover
+
+
+@no_type_check
+def init_logger(name=None, level=None):
+    """Initialize module level logger"""
+    global log  # pylint: disable=global-statement
+
+    log_format = {
+        'format': '%(asctime)s %(levelname)s [%(name)s]: %(message)s',
+        'datefmt': TS_FORMAT_LOG,
+        # 'filename': LOG_PATH,
+        'level': LOG_LEVEL if level is None else level,
+    }
+    logging.Formatter.formatTime = formatTime_RFC3339
+    logging.basicConfig(**log_format)
+    log = logging.getLogger(APP_ENV if name is None else name)
+    log.propagate = True
+
+
+init_logger(name=APP_ENV, level=logging.DEBUG if DEBUG else None)
